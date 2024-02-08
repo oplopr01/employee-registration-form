3533a4deb030a5add075216a4a8fd7786faaf043
lakhan <oplopr01@gmail.com>
2024-02-08 17:27:05 +0530

    created "CreateUser" component. 3 input fields and one button to submit input data. used useState hook to create controlled form.
    user input will be saved in respactive states, depend on onChange event,

--- src/Components/pages/CreateUser.jsx
+++ src/Components/pages/CreateUser.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from "../home.module.css"
 
 const CreateUser = () => {
+    let [name, setName] = useState("")
+    let [salary, setSalary] = useState(0)
+    let [company, setCompany] = useState("")
+
+    let formHandler= (e)=>{
+        e.preventDefault()
+        console.log(name, salary, company);
+    }
+
   return (
     <div>
-        <h1>create user page</h1>
+    
+        <div id={style.formContainer}>
+      <form action="">
+        <div>
+        <label htmlFor="">Name: </label>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+        <br />
+        </div>
+        <div> <label htmlFor="">Salary: </label>
+        <input type="text" value={salary} onChange={(e)=>setSalary(e.target.value)} />
+        <br />
+       </div>
+        <div> <label htmlFor="">Company:   </label>
+        <input type="text" value={company} onChange={(e)=>setCompany(e.target.value)} />
+        </div>
+        <br />
+          <button onClick={formHandler}>SUBMIT</button>
+      </form>
+    </div>
+    
     </div>
   )
 }
